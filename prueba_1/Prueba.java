424908731b4a94c85d70700591ccfe242136c0dc
Johann Bogota <johann.bogota@mail.escuelaing.edu.co>
2021-05-13 16:44:42 -0500

    prueba python

--- prueba_1/Prueba.java
+++ prueba_1/Prueba.java
@@ -1,4 +1,4 @@
-package prueba;
+package prueba_1;
 
 public class Prueba {
 
---
+++ prueba_1/python.py
@@ -0,0 +1,11 @@
+def main():
+    for i in range(1,101):
+        if i%3==0 and i%5==0:
+            print("mareigua")
+        elif i%3==0:
+            print("mare")
+        elif i%5==0:
+            print("igua")
+
+
+main()
